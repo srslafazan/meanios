58acbfb856f6abe306f85e5a7dc932f895f77817
Shain Lafazan <srslafazan@gmail.com>
2015-06-17 14:40:53 -0700

    added functionality for iOS view

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UITableViewDataSource {
 
     var friends = [Friend]()
     
     override func viewDidLoad() {
+        println("hi")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        if let urlToReq = NSURL(string: "http://localhost:8000/friends") {
+
+        if let urlToReq = NSURL(string: "http://192.168.1.125:8000/friends") {
             if let data = NSData(contentsOfURL: urlToReq) {
                 let arrOfFriends = parseJSON(data)
-                
+
                 for friend in arrOfFriends {
                     let object = friend as! NSDictionary
                     
@@ -36,9 +37,31 @@ class ViewController: UIViewController {
     func parseJSON(inputData: NSData) -> NSArray {
         var error: NSError?
         var arrOfObjects = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as! NSArray
+        println(arrOfObjects)
         return arrOfObjects
     }
 
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+            let dequeued: AnyObject = tableView.dequeueReusableCellWithIdentifier("FriendCell", forIndexPath: indexPath)
+            
+            let cell = dequeued as! FriendCell
+            let friend = friends[indexPath.row]
+            
+            cell.friendName.text = friend.friendName
+            cell.friendAge.text = String(stringInterpolationSegment: friend.friendAge)
+        
+            return cell
+
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
