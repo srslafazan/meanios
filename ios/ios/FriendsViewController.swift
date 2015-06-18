3107d75787b0ba819b00863049dcf53b6811ede1
Rod Woodman <roderickwoodman@gmail.com>
2015-06-17 19:20:32 -0700

    added segue back to home from insert

--- ios/ios/FriendsViewController.swift
+++ ios/ios/FriendsViewController.swift
@@ -10,21 +10,23 @@ import Foundation
 import UIKit
 
 class FriendsViewController: UIViewController {
+    
     @IBOutlet weak var nameTextField: UITextField!
+    
     @IBOutlet weak var ageTextField: UITextField!
+    
     @IBAction func insertButtonPressed(sender: UIButton) {
-        if let urlToReq = NSURL(string: "http://192.168.1.125:8000/friends") {
+        
+        if let urlToReq = NSURL(string: "http://192.168.1.7:8000/friends") {
+            
             var request: NSMutableURLRequest = NSMutableURLRequest(URL: urlToReq)
             request.HTTPMethod = "POST"
             var bodyData = "name=\(nameTextField.text)&age=\(ageTextField.text)"
-            
-            
-            
+    
             request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
-            println("hello World")
+
             NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
                 (response, data, error) in
-//                println(NSString(data: data, encoding: NSUTF8StringEncoding))
             }
         }
     }
--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class ViewController: UITableViewController, UITableViewDataSource {
 
+    @IBAction func insertNewFriendAndGoBackToFriends(segue:UIStoryboardSegue) {
+        
+        let sourceVC = segue.sourceViewController as! FriendsViewController
+        
+        // create new instance of Friend and append to the Friend Array
+        let friend = Friend(name: sourceVC.nameTextField.text, age: sourceVC.ageTextField.text.toInt())
+        friends.append(friend)
+        
+        tableView.reloadData()
+    }
+    
     var friends = [Friend]()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view, typically from a nib.
-
-        if let urlToReq = NSURL(string: "http://192.168.1.125:8000/friends") {
+        if let urlToReq = NSURL(string: "http://192.168.1.7:8000/friends") {
 
             if let data = NSData(contentsOfURL: urlToReq) {
                 let arrOfFriends = parseJSON(data)
-                println(1)
-                print(arrOfFriends)
                 for friend in arrOfFriends {
                     
                     if let object = friend as? NSDictionary {
                         if let friendName = object["name"] as? String {
                             if let friendAge = object["age"] as? Int {
                                 let friend = Friend(name: friendName, age: friendAge)
-                                println(friend)
                                 friends.append(friend)
                             }
                         }
@@ -41,19 +48,15 @@ class ViewController: UITableViewController, UITableViewDataSource {
     
     func parseJSON(inputData: NSData) -> NSArray {
         var error: NSError?
-                        println(3)
         var arrOfObjects = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as! NSArray
         return arrOfObjects
     }
 
-    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                        println(4)
         return friends.count
 
     }
     
-
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
             let dequeued: AnyObject = tableView.dequeueReusableCellWithIdentifier("FriendCell", forIndexPath: indexPath)
@@ -63,11 +66,9 @@ class ViewController: UITableViewController, UITableViewDataSource {
             
             cell.friendName.text = friend.friendName
             cell.friendAge.text = String(stringInterpolationSegment: friend.friendAge)
-                        println(5)
             return cell
 
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
